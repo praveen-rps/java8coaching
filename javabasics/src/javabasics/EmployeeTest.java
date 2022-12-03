8ce63891f8dc4f5733d8f4e2d288838ea93516e2
praveen-rps <109707042+praveen-rps@users.noreply.github.com>
2022-12-03 23:15:41 +0530

    Add files via upload

---
+++ javabasics/src/javabasics/EmployeeTest.java
@@ -0,0 +1,32 @@
+package javabasics;
+
+public class EmployeeTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=100, b=20;
+		System.out.println(a);
+		String name="praveen", city="Hyderabad";
+		System.out.println(name);
+		boolean flag1=true, flag2=false;
+		name="kumar";
+		System.out.println(name);
+		a=200;
+		System.out.println(a);
+		
+		int x[] = new int[5];
+		x[0]=10;
+		x[1]=20;
+		x[2]=30;
+		System.out.println(x[1]);
+		System.out.println(x[2]);
+		x[4]=x[1]+x[2];
+		System.out.println(x[4]);
+		
+		
+		
+		
+
+	}
+
+}
